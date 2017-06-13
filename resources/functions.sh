8591c9444603f6881ec1342815cab8ef4399bf5f
Liviu Balan <liv_romania@yahoo.com>
2017-06-13 09:35:35 +0300

    Add copyright headers

--- resources/functions.sh
+++ resources/functions.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#
+# (c) Liviu Balan <liv_romania@yahoo.com>
+# http://www.liviubalan.com/
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+
 # Escape string used in sed command (search term).
 # For more info see:
 # https://robots.thoughtbot.com/sed-102-replace-in-place
